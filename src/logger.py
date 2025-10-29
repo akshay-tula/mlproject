b8025a23718c060910d4babfb1510e776caa2e17
akshay-tula <akshay.tula@gmail.com>
2025-10-29 15:54:36 +0530

    EDA & Problem Statement

--- src/logger.py
+++ src/logger.py
@@ -1,22 +1,28 @@
 import logging
 import os
 from datetime import datetime
-import sys 
+import sys
 
 # 1. Define the log file NAME
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-# 2. Define the log folder PATH (relative to the script execution directory)
-LOGS_PATH = os.path.join(os.getcwd(), "logs") 
+LOGS_PATH = os.path.join(os.getcwd(), "logs")
 
 # 3. Create the log folder if it doesn't exist
 os.makedirs(LOGS_PATH, exist_ok=True)
 
-# 4. Define the complete log file path
-LOG_FILE_PATH = os.path.join(LOGS_PATH, LOG_FILE) # <--- Combine folder and file name
+LOG_FILE_PATH = os.path.join(LOGS_PATH, LOG_FILE)
+
+LOG_FORMAT = "[%(asctime)s] %(lineno)d: %(name)s - %(levelname)s - %(message)s"
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[%(asctime)s ] %(lineno)d: %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler(LOG_FILE_PATH),
+              logging.StreamHandler(sys.stdout)
+              ],
+    format=LOG_FORMAT, 
     level=logging.INFO
 )
+logging = logging.getLogger("MLPROJECT_LOGGER")
+
+if __name__ == "__main__":
+    logging.info("Logging has started successfully.")
