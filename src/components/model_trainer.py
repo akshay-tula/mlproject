0c12b7cbd648556f1ef6468112b4a97b3383b9e9
akshay-tula <akshay.tula@gmail.com>
2025-11-04 16:15:36 +0530

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,8 +47,50 @@ class ModelTrainer:
                 "Adaboost Regressor": AdaBoostRegressor()
                 }
             
+            # --- HYPERPARAMETER GRIDS ADDED HERE ---
+            params = {
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neigbour Regressor":{},
+                "XGBoost Regressor":{
+                   
+                    'tree_method': ['hist'],
+                    'device': ['cuda'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoost Regressor":{
+                    
+                    'task_type': ['GPU'], 
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "Adaboost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
             model_report: dict= evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                                models=models)
+                                                models=models,param=params)
             
             ##To get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
